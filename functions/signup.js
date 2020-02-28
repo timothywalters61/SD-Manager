df5a884d380e984077ee238a523c1a726ee2bc55
Simphiwe Zulu <szulu107@gmail.com>
2020-02-28 09:52:57 +0200

    signup.js added

---
+++ functions/signup.js
@@ -0,0 +1,8 @@
+/**
+ * @author Simphiwe Zulu
+ * @description This file contains all the Cloud Functions that handle the sign up
+ *              feature of the platform
+ */
+
+const admin = require('firebase-admin');
+const functions = require('firebase-functions');
