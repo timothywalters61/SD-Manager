74d54b7d3bb6c44f8ec8246f8f924c8da52bbcf0
Simphiwe Zulu <szulu107@gmail.com>
2020-02-29 08:18:59 +0200

    userCheck() added but not completed

---
+++ public/resources/js/session.js
@@ -0,0 +1,24 @@
+/**
+ * @description This file controls the user's session as well as which pages the user
+ *              has access to based on their access level.
+ */
+
+/**
+ * This function will be called every time a page is loaded to check whether there's
+ * a user logged in or not. If a user is logged in, then check if they are in the
+ * correct section of the web app. I
+ */
+function userCheck() {
+	firebase.auth().onAuthStateChanged(function(user) {
+		if (user) {
+			// User is signed in.
+		} else {
+			// No user is signed in.
+			var path = getPathname(); // the name of the current page
+			// check if the current page isn't the home page or sign up page
+			if (path != 'signup.html' || path != 'index.html') {
+				// user is not is the correct section
+			}
+		}
+	});
+}
