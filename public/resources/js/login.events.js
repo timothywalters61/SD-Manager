8c5825f74f90386fc1950d4a521ab96e97f398d2
Simphiwe Zulu <szulu107@gmail.com>
2020-02-29 21:57:20 +0200

    event listener added

--- public/resources/js/login.events.js
+++ public/resources/js/login.events.js
@@ -2,3 +2,16 @@
  * @description This page contains all the event handlers that are related to the
  *              login section of the home page
  */
+
+var ids = ['login-em', 'login-pa'];
+
+ids.forEach((id, index) => {
+	var domElement = document.getElementById(id);
+	domElement.addEventListener('keyup', function(event) {
+		// 13 is the "Enter" key on the keyboard
+		if (event.keyCode == 13) {
+			event.preventDefault(); // cancel the default action
+			document.getElementById('signup-button-next').click(); // trigger the button element
+		}
+	});
+});
