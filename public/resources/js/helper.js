51eedc687177c5bd8b853806ae3571470a708fdb
Simphiwe Zulu <szulu107@gmail.com>
2020-02-29 08:27:21 +0200

    getter and setter functions added

---
+++ public/resources/js/helper.js
@@ -0,0 +1,39 @@
+/**
+ * @description This file contains all functions that will be used to simplify
+ *              the code.
+ */
+
+/* =============================== Getters ================================ */
+/**
+ * Returns the value of the element specified by id
+ * @param {String} id
+ */
+function getElementValue(id) {
+	return document.getElementById(id).value;
+}
+
+/**
+ * Returns the current URL pathname
+ */
+function getPathname() {
+	return location.pathname;
+}
+
+/* =============================== Setters ================================ */
+
+/**
+ * Sets the value of the element specified by id to val
+ * @param {String} id
+ * @param {String} val
+ */
+function setElementValue(id, val) {
+	document.getElementById(id).value = val;
+}
+
+/**
+ * Sets the current URL pathname to 'pathname'
+ * @param {String} pathname
+ */
+function setPathname(pathname) {
+	location.pathname = pathname;
+}
