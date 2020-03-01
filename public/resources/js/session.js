15d0b742b13eaad287a96741c38f3b0d122cd3e9
Simphiwe Zulu <szulu107@gmail.com>
2020-03-01 17:10:26 +0200

    logout() added

--- public/resources/js/session.js
+++ public/resources/js/session.js
@@ -4,6 +4,17 @@
  */
 
 /**
+ * Signs the user out of their account and takes them to the home page
+ */
+function logout() {
+	auth.signOut()
+		.then(() => {
+			location.pathname = 'index.html';
+		})
+		.catch(onError);
+}
+
+/**
  * This function will be called every time a page is loaded to check whether there's
  * a user logged in or not. If a user is logged in, then check if they are in the
  * correct section of the web app. I
@@ -19,6 +30,8 @@ function userCheck() {
 			if (path != 'index.html') {
 				// user is not is the correct section
 				setPathname('index.html');
+			} else {
+				showHome();
 			}
 		}
 	});
