8b084cd64e7df72c82372a7618e97535da3391ae
Simphiwe Zulu <szulu107@gmail.com>
2020-03-01 12:23:11 +0200

    event listener added

--- public/resources/js/signup.events.js
+++ public/resources/js/signup.events.js
@@ -2,3 +2,23 @@
  * @description This file contains event handlers that are related to the sign up
  *              form of the home page.
  */
+
+var ids = [
+	'signup-em',
+	'signup-fn',
+	'signup-ln',
+	'signup-un',
+	'signup-un-id',
+	'signup-pa',
+	'signup-co-pa'
+];
+ids.forEach((id, index) => {
+	var domElement = document.getElementById(id);
+	domElement.addEventListener('keyup', function(event) {
+		// 13 is the "Enter" key on the keyboard
+		if (event.keyCode == 13) {
+			event.preventDefault(); // cancel the default action
+			document.getElementById('signup-button').click(); // trigger the button element
+		}
+	});
+});
