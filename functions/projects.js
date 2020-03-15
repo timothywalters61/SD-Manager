e3434108c93e788a7cc1520a19b29390ae04ff5a
Simphiwe Zulu <szulu107@gmail.com>
2020-03-15 16:44:28 +0200

    addProjectToUserDocument() added

--- functions/projects.js
+++ functions/projects.js
@@ -12,6 +12,30 @@ const functions = require('firebase-functions');
 
 /* ========================================= Local Functions ======================================= */
 /**
+ * Creates a project document in a user's projects subcollection
+ * @param {string} projectid
+ * @param {string} projectName
+ * @param {string} projectDescription
+ * @param {string} projectRole
+ * @param {string} uid
+ */
+function addProjectToUserDocument(
+	projectid,
+	projectName,
+	projectDescription,
+	projectRole,
+	uid
+) {
+	var data = {
+		name: projectName,
+		description: projectDescription,
+		role: projectRole
+	};
+
+	return docs.createDoc('users/' + uid + '/projects/' + projectid, data);
+}
+
+/**
  * Creates a document in the Projects collection with data as its content
  * @param {object} data
  */
