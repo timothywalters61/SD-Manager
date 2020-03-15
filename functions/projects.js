55cdee6f848160d56b9e566cc94ad1f76ca0b6bd
Simphiwe Zulu <szulu107@gmail.com>
2020-03-15 16:36:31 +0200

    createProjectDocument() added

--- functions/projects.js
+++ functions/projects.js
@@ -3,6 +3,18 @@
  *              the database.
  */
 
+const admin = require('firebase-admin');
+const docs = require('./docs');
+const errors = require('./errors');
+const functions = require('firebase-functions');
+
 /* =========================================== Exports ============================================= */
 
 /* ========================================= Local Functions ======================================= */
+/**
+ * Creates a document in the Projects collection with data as its content
+ * @param {object} data
+ */
+function createProjectDocument(data) {
+	return docs.addDoc('projects', data);
+}
