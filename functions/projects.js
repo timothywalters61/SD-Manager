0a87281998434408229aac5a48c9de5d1fedc93b
Simphiwe Zulu <szulu107@gmail.com>
2020-03-15 16:26:16 +0200

    projects.js created

---
+++ functions/projects.js
@@ -0,0 +1,8 @@
+/**
+ * @description This file contains all the functions that use the 'projects' document in
+ *              the database.
+ */
+
+/* =========================================== Exports ============================================= */
+
+/* ========================================= Local Functions ======================================= */
