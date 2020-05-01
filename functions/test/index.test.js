510a90046079bc8cf340cd82367621d6ba756ff5
Simphiwe <szulu107@gmail.com>
2020-05-01 14:34:53 +0200

    imports added

--- functions/test/index.test.js
+++ functions/test/index.test.js
@@ -2,3 +2,14 @@
  * @description This file contains all the unit tests that will be used to test the
  *              Cloud Functions
  */
+// At the top of test/index.test.js
+const test = require('firebase-functions-test')();
+
+// If index.js calls admin.initializeApp at the top of the file,
+// we need to stub it out before requiring index.js. This is because the
+// functions will be executed as a part of the require process.
+// Here we stub admin.initializeApp to be a dummy function that doesn't do anything.
+adminInitStub = sinon.stub(admin, 'initializeApp');
+// Now we can require index.js and save the exports inside a namespace called myFunctions.
+const myFunctions = require('../index');
+
