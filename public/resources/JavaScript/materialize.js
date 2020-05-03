b32a47d4a48a7c396816a86a9b2b19f663ab4cf1
JCR1999 <55395363+JCR1999@users.noreply.github.com>
2020-05-03 16:29:56 +0200

    Delete materialize.js

--- public/resources/JavaScript/materialize.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-
-    var modals = document.querySelectorAll('.modal');
-    M.Modal.init(modals);
-
-    var sideNav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(sideNav);
-});
