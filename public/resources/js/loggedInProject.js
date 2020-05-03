33dbff6f8c1e861b81f0b67a268eff52476cb655
JCR1999 <55395363+JCR1999@users.noreply.github.com>
2020-05-03 16:40:43 +0200

    Add files via upload

---
+++ public/resources/js/loggedInProject.js
@@ -0,0 +1,64 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyB1akwPd-xOMCgU9_Bc6OqdTObTp10Sb5k",
+    authDomain: "scrum-manager-91e13.firebaseapp.com",
+    databaseURL: "https://scrum-manager-91e13.firebaseio.com",
+    projectId: "scrum-manager-91e13",
+    storageBucket: "scrum-manager-91e13.appspot.com",
+    messagingSenderId: "332929508306",
+    appId: "1:332929508306:web:5f1773dc956813db641e0b"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.firestore();
+const projectID = localStorage.getItem("docID");
+const ownerID = localStorage.getItem("ownerID");
+
+auth.onAuthStateChanged(user => {
+    if (user) {
+        console.log("user logged in: ", user);
+        //console.log(data);
+
+        //only project owner can open this page
+
+        if(user.uid != ownerID){
+            window.location.href = "userHome.html";
+        }
+
+        //set up sidenav
+
+        var data = [user.displayName, user.email];
+        setUpSideNav(data);
+
+        //populate team dropdown
+
+        db.collection("Projects").doc(projectID)
+            .onSnapshot(function(doc) {
+                if (doc.exists) {
+                    console.log(doc.data().Team);
+                    setUpTeam(doc.data().Team);
+                } else {
+                    console.log("project does not exist");
+                }
+            });
+    } else {
+        console.log("user logged out");
+        window.location.href = "index.html";
+    }
+});
+
+//logout
+
+const logout = document.querySelector("#logout");
+logout.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    auth.signOut().then(() => {
+        //console.log("user has signed out");
+    })
+        .catch(function (error) {
+            console.log("user failed to sign out because of error: ", error);
+            alert(error.message);
+        });
+});
