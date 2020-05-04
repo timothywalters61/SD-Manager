ae24b8fe0029af4eda87c334e4f78dbbd2d6f6aa
Simphiwe Zulu <szulu107@gmail.com>
2020-05-04 15:35:49 +0200

    imports added

--- functions/auth.js
+++ functions/auth.js
@@ -2,3 +2,7 @@
  * @description This file contains all functions that make use of Firebase Auth that
  *              can be used in other parts of the Cloud Functions.
  */
+
+const admin = require('firebase-admin');
+
+/* ============================ Export Functions =========================== */
