3b1634ba6d75115ce8099f3dac5f2be61d836833
Simphiwe Zulu <szulu107@gmail.com>
2020-05-04 15:37:11 +0200

    getUser() added

--- functions/auth.js
+++ functions/auth.js
@@ -6,3 +6,10 @@
 const admin = require('firebase-admin');
 
 /* ============================ Export Functions =========================== */
+/**
+ * Gets user data for the user corresponding to a given uid
+ * @param {String} uid The uid corresponding to the user whose data to fetch
+ */
+exports.getUser = (uid) => {
+	return admin.auth().getUser(uid);
+};
