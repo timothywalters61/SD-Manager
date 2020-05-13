1e7a51f22b6964481f783dac0efef18a22265a7e
Simphiwe Zulu <szulu107@gmail.com>
2020-05-13 02:44:16 +0200

    createSprintDataObject() added

--- functions/sprints.js
+++ functions/sprints.js
@@ -10,3 +10,16 @@ const functions = require('firebase-functions');
 /* ===================================== Exports ====================================== */
 
 /* ================================== Local Functions ================================= */
+/**
+ * Returns a map of field and values that will be when creating a new sprint document.
+ * @param {*} end
+ * @param {String} name
+ * @param {*} start
+ */
+function createSprintDataObject(end, name, start) {
+	return {
+		end_date: end,
+		name: name,
+		start_date: start,
+	};
+}
