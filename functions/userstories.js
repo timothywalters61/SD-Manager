c36d5d2478a782d5eff0f8ec759867b7769a9863
Simphiwe Zulu <szulu107@gmail.com>
2020-05-13 02:20:58 +0200

    imports added

--- functions/userstories.js
+++ functions/userstories.js
@@ -3,5 +3,9 @@
  *              stories that will be saved in the 'backlog'
  */
 /* ===================================== Imports ====================================== */
+const auth = require('./auth');
+const docs = require('./docs');
+const errors = require('./errors');
+const functions = require('firebase-functions');
 /* ===================================== Exports ====================================== */
 /* ================================== Local Functions ================================= */
