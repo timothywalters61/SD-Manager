fedc17f8b0ecd348242e6c46bacc3790b8f8477c
Simphiwe Zulu <szulu107@gmail.com>
2020-05-13 03:11:26 +0200

    createProjectDocumentDataObject() added

--- public/resources/js/projects.js
+++ public/resources/js/projects.js
@@ -2,3 +2,18 @@
  * @description This file contains all the code taht will be used in the projects
  *              section.
  */
+
+/**
+ * Creates a map of field and value pairs that will be used to create a project
+ * document.
+ * @param {String} name
+ * @param {String} description
+ * @param {String} repository
+ */
+function createProjectDocumentDataObject(name, description, repository) {
+	return {
+		description: description,
+		name: name,
+		repository: repository,
+	};
+}
