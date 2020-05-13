417568c5fa7220e3d4e4c03f44661cd79b7991c1
Simphiwe Zulu <szulu107@gmail.com>
2020-05-13 02:13:10 +0200

    imports added

--- functions/sprints.js
+++ functions/sprints.js
@@ -2,3 +2,11 @@
  * @description This file contains all functions that interact with the sprint documents
  *              in the 'sprints' collection
  */
+/* ===================================== Imports ====================================== */
+const auth = require('./auth');
+const docs = require('./docs');
+const errors = require('./errors');
+const functions = require('firebase-functions');
+/* ===================================== Exports ====================================== */
+
+/* ================================== Local Functions ================================= */
