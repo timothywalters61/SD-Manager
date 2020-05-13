84fb9231b8b07d6e3b3234823a9dacad9d384c6e
Simphiwe Zulu <szulu107@gmail.com>
2020-05-13 03:38:29 +0200

    setDoc() added

--- public/resources/js/helper.docs.js
+++ public/resources/js/helper.docs.js
@@ -12,3 +12,14 @@
 function addDoc(ref, data) {
 	return firestore.collection(ref).add(data);
 }
+
+/**
+ * Creates or overwrites a single document specified by doc which is found in a
+ * collection specified by col
+ * @param {String} col
+ * @param {Object} data
+ * @param {String} doc
+ */
+function setDoc(col, data, doc) {
+	return firestore.collection(col).doc(doc).set(data);
+}
