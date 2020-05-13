5661d4333274eb7c9b4b2825b04e745b9ef6d953
Simphiwe Zulu <szulu107@gmail.com>
2020-05-13 03:30:19 +0200

    addDocument() added

--- public/resources/js/helper.docs.js
+++ public/resources/js/helper.docs.js
@@ -2,3 +2,13 @@
  * @description This file contains all the functions that will be used to handle Cloud
  *              Firestore functionality
  */
+
+/**
+ * Adds a new document to a collection specified by ref that will store the contents
+ * of data
+ * @param {String} ref
+ * @param {Object} data
+ */
+function addDocument(ref, data) {
+	return firestore.collection(ref).add(data);
+}
