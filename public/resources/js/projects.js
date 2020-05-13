59c7cdcff7db1295ac0aa723d214efa665be8a74
Simphiwe Zulu <szulu107@gmail.com>
2020-05-13 03:24:07 +0200

    createProjectMemberDataObject() added

--- public/resources/js/projects.js
+++ public/resources/js/projects.js
@@ -4,7 +4,22 @@
  */
 
 /**
- * Creates a map of field and value pairs that will be used to create a project
+ * Returns a map of fields and values that will be used to create a member document
+ * in a project's members subcollection.
+ * @param {String} email
+ * @param {String} name
+ * @param {String} role
+ */
+function createProjectMemberDataObject(email, name, role) {
+	return {
+		email: email,
+		name: name,
+		role: role,
+	};
+}
+
+/**
+ * Returns a map of field and value pairs that will be used to create a project
  * document.
  * @param {String} name
  * @param {String} description
