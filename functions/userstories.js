32679d07991f2991a8d5fa1a0465646520926567
Simphiwe Zulu <szulu107@gmail.com>
2020-05-13 02:20:02 +0200

    userstories.js created

---
+++ functions/userstories.js
@@ -0,0 +1,7 @@
+/**
+ * @description This file contains all the functions that will be used to handle user
+ *              stories that will be saved in the 'backlog'
+ */
+/* ===================================== Imports ====================================== */
+/* ===================================== Exports ====================================== */
+/* ================================== Local Functions ================================= */
