5b57ff4799f90484b8ad3e62e50c33ee72577de4
Simphiwe Zulu <szulu107@gmail.com>
2020-05-13 02:52:54 +0200

    createSprints() added

--- functions/sprints.js
+++ functions/sprints.js
@@ -8,7 +8,21 @@ const docs = require('./docs');
 const errors = require('./errors');
 const functions = require('firebase-functions');
 /* ===================================== Exports ====================================== */
+/**
+ * Creates a new sprint document in a project's sprints subcollection
+ */
+exports.createSprint = functions.https.onCall((data, context) => {
+	var end = data.end;
+	var name = data.nam;
+	var projectid = data.pid;
+	var start = data.sta;
 
+	var data = createSprintDataObject(end, name, start);
+
+	return createSprintDocument(data, projectid).then((value) => {
+		return { result: 'Success' };
+	});
+});
 /* ================================== Local Functions ================================= */
 /**
  * Returns a map of field and values that will be when creating a new sprint document.
