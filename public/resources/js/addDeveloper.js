aeb6717de24de86c65087e7fc9ec3bbce3211b5e
Simphiwe Zulu <szulu107@gmail.com>
2020-05-16 02:02:56 +0200

    add developer modified to fit database changes

--- public/resources/js/addDeveloper.js
+++ public/resources/js/addDeveloper.js
@@ -1,43 +1,48 @@
-const addForm = document.querySelector("#add-form");
+const addForm = document.querySelector('#add-form');
 
 addForm.addEventListener('submit', (e) => {
-    e.preventDefault();
+	e.preventDefault();
 
-    const dev = addForm['add-email'].value;
-    var user = auth.currentUser;
+	const dev = addForm['add-email'].value;
+	var user = auth.currentUser;
 
-
-    db.collection("Projects").doc(projectID)
-        .get()
-        .then((doc) => {
-            if (doc.exists) {
-                if (doc.data().Team.includes(dev)) {
-                    console.log("in the team");
-                    alert("developer is already in the team");
-                } else {
-                    db.collection("Users").where("userEmail", "==", dev).get()
-                        .then((querySnapshot) => {
-                            querySnapshot.forEach((doc) => {
-                                if (doc.exists) {
-                                    db.collection("Invites").doc().set({
-                                        inviteFromID: user.uid,
-                                        inviteToID: doc.id,
-                                        inviteFromEmail: user.email,
-                                        projectID: projectID
-                                    }).then(() => {
-                                        alert("invite sent");
-                                        addForm.reset();
-                                    });
-                                } else {
-                                    alert("user does not exist");
-                                }
-                            });
-                        }).catch((error) => {
-                            alert("error getting documents, ", error);
-                        });
-                }
-            } else {
-                console.log("project does not exist");
-            }
-        });
+	getDoc('projects', projectID).then((doc) => {
+		if (doc.exists) {
+			if (doc.data().team.includes(dev)) {
+				console.log('in the team');
+				alert('Developer is already in the team');
+			} else {
+				var cond = {
+					fieldPath: 'email',
+					opStr: '==',
+					value: dev,
+				};
+				collectionWhere('users', cond)
+					.get()
+					.then((querySnapshot) => {
+						querySnapshot.forEach((doc) => {
+							if (doc.exists) {
+								data = {
+									from_id: user.uid,
+									to_id: doc.id,
+									from_email: user.email,
+									project_id: projectID,
+								};
+								addDoc('invites', data).then(() => {
+									alert('invite sent');
+									addForm.reset();
+								});
+							} else {
+								alert('user does not exist');
+							}
+						});
+					})
+					.catch((error) => {
+						alert('error getting documents, ', error);
+					});
+			}
+		} else {
+			console.log('project does not exist');
+		}
+	});
 });
