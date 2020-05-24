238e04e79e39dc3877a16c6a6bef485ad7763513
Simphiwe <szulu107@gmail.com>
2020-05-24 17:22:27 +0200

    document reference retrieved

--- public/resources/js/sprints.populate.js
+++ public/resources/js/sprints.populate.js
@@ -1,3 +1,10 @@
 /**
  * @description Populates the SprintsPage.html file with data
  */
+const projectID = localStorage.getItem('docID');
+const sprintID = localStorage.getItem('sprintID')
+getDocumentReference(`projects/${projectID}/sprints/${sprintID}`).onSnapshot((snapshot)=> {
+
+}).catch((error) =>
+    alert('Could not retrive user story due to an error', error)
+);
