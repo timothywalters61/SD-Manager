6dedeffa06321569f925478c7040b0379ab205e8
phediso99 <61417155+phediso99@users.noreply.github.com>
2020-08-07 03:29:57 +0200

    Update createSprint.js

--- public/public/resources/js/createSprint.js
+++ public/public/resources/js/createSprint.js
@@ -6,23 +6,31 @@ startSprint.addEventListener('submit', (e) => {
     const sprintName = startSprint['sprint-name'].value;
     const startDate = startSprint['startDate'].value;
     const endDate = startSprint['endDate'].value;
+    var today = Date.now;
+
+    if (startDate < today) {
+        toast("startdate already past");
+    }
+
+
 
     db.collection("projects").doc(projectID).collection("sprints").add({
-        name: sprintName,
-        start: startDate,
-        end: endDate
-    })
-    .then((doc) => {
-        localStorage.setItem("currentSprintID", doc.id);
-        db.collection("projects").doc(projectID).update({
-            "Sprints": "started"
+            name: sprintName,
+            start: startDate,
+            end: endDate
         })
-        .then(() => {
-            startSprint.reset();
-            window.location.href = "Sprint.html";
+        .then((doc) => {
+            localStorage.setItem("currentSprintID", doc.id);
+            db.collection("projects").doc(projectID).update({
+                    "Sprints": "started"
+                })
+                .then(() => {
+                    startSprint.reset();
+                    window.location.href = "Sprint.html";
+                });
+        })
+        .catch((error) => {
+            alert("an error occured", error);
         });
-    })
-    .catch((error) => {
-        alert("an error occured", error);
-    });
+
 });
