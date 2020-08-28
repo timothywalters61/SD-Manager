97237a5d804613552b47bb278dd01a4494a2b221
Simphiwe <szulu107@gmail.com>
2020-08-28 16:37:08 +0200

    Username validation done

--- public/public/resources/js/signUp.js
+++ public/public/resources/js/signUp.js
@@ -15,6 +15,7 @@ const db = firebase.firestore();
 var user = auth.currentUser;
 const typingTimeInterval = 3000;
 var typingTimer;
+var isUsernameUnique = false;
 
 auth.onAuthStateChanged(user => {
     if (user) {
@@ -32,6 +33,7 @@ auth.onAuthStateChanged(user => {
 const signupForm = document.querySelector("#signup-form");
 signupForm.addEventListener('submit', (e) => {
     e.preventDefault();
+    clearTimeout(typingTimer);
 
     const signUpEmail = signupForm['signup-email'].value;
     const signUpUsername = signupForm['signup-username'].value;
@@ -40,30 +42,44 @@ signupForm.addEventListener('submit', (e) => {
     const signUpPassword = signupForm['signup-password'].value;
     const signUpConfirmpassword = signupForm['signup-confirmpassword'].value;
 
-    if ((signUpPassword === signUpConfirmpassword) && (signUpPassword.length >= 6)) {
+    db.collection('users').where('userDisplayName', '==', signUpUsername).onSnapshot((query) => {
+        if (!query.empty) {
+            // alert user to enter different user name
+            isUsernameUnique = false;
+        } else {
+            isUsernameUnique = true;
+            console.log("Username is unique");
+        }
+    });
 
-        auth.createUserWithEmailAndPassword(signUpEmail, signUpPassword).then(cred => {
-            return cred.user.updateProfile({
-                displayName: signUpUsername
-            });
-        }).then(() => {
-            return db.collection("users").doc(user.uid).set({
-                userEmail: signUpEmail,
-                userDisplayName: signUpUsername,
-                userFirstName: signUpFirstName,
-                userLastName: signUpLastName
+    if ((signUpPassword === signUpConfirmpassword) && (signUpPassword.length >= 6)) {
+        if (isUsernameUnique)  {
+            auth.createUserWithEmailAndPassword(signUpEmail, signUpPassword).then(cred => {
+                user = cred.user;
+                return cred.user.updateProfile({
+                    displayName: signUpUsername
+                });
+            }).then(() => {
+                return db.collection("users").doc(user.uid).set({
+                    userEmail: signUpEmail,
+                    userDisplayName: signUpUsername,
+                    userFirstName: signUpFirstName,
+                    userLastName: signUpLastName
+                })
+            }).then(() => {
+                signupForm.reset();
+                window.location.href = "userHome.html";
             })
-        }).then(() => {
-            signupForm.reset();
-            window.location.href = "userHome.html";
-        })
-            .catch(function (error) {
-                console.log("failed to authenticate user because of error: ", error);
-                toast(error);
-            });
+                .catch(function (error) {
+                    console.log("failed to authenticate user because of error: ", error);
+                    toastError(error);
+                });
+        } else {
+            toast("Username already exists");
+        }
     } else {
         console.log("passwords must match and be 6 or more characters long!!!");
-        toast("passwords must match and be 6 or more characters long!!!");
+        toast("Passwords must match and be 6 or more characters long.");
     }
 });
 
@@ -71,9 +87,20 @@ signupForm.addEventListener('submit', (e) => {
 
 const usernameInput = document.querySelector("#signup-username");
 usernameInput.addEventListener('keyup', (e) => {
-    clearTimeout(typingTimeInterval);
+    clearTimeout(typingTimer);
     typingTimer = setTimeout(() => {
-       // check if username exists 
+        // check if username exists
+        const username = signupForm['signup-username'].value;
+        db.collection('users').where('userDisplayName', '==', username).onSnapshot((query) => {
+            if (!query.empty) {
+                // alert user to enter different user name
+                toastError("Username already exists");
+                console.log("Username already exists");
+            } else {
+                isUsernameUnique = true;
+                console.log("Username is unique");
+            }
+        });
     }, typingTimeInterval);
 });
 
