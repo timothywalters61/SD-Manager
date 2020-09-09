30b8a9185506909e7c219f9acca2a8782923c788
mikayla-anne <61417406+mikayla-anne@users.noreply.github.com>
2020-09-09 12:26:38 +0200

    drag+drop tasks
    there will be errors since some data being looked for does not exist in firebase db.

--- public/public/resources/js/getTasks.js
+++ public/public/resources/js/getTasks.js
@@ -17,6 +17,8 @@ const projectID = localStorage.getItem("docID");
 const ownerID = localStorage.getItem("ownerID");
 const currentSprintID = localStorage.getItem("currentSprintID");
 const projectName = localStorage.getItem("docName");
+const userStoryID = localStorage.getItem("userStoryID");
+console.log(userStoryID);
 
 const projectTitle = document.querySelector("#PageHeading");
 let heading = `<p>Project: ${projectName}</p>`;
@@ -74,64 +76,160 @@ const IP= document.getElementById("In Progress");
 const C = document.getElementById("Completed");
 
 let wholeDiv;
-let userIDs = new Map();
+let taskIDs = new Map();
 
-const tasks = document.querySelectorAll('.tasks');
-const categories = document.querySelectorAll('.categories');
+//get tasks from db
 
-let dragTask= null;
+db.collection("projects").doc(projectID).collection("sprints").doc(currentSprintID).collection("backlog").doc(userStoryID).collection("tasks").get().then(function(querySnapshot) {
+    querySnapshot.forEach(function(doc) {
 
-for(let a = 0; a < tasks.length;a++) {
- const task = tasks[a];
+        let taskName = doc.data().name;
+        let taskAssigned = doc.data().assigned_to;
+        let status = parseInt(doc.data().status);
+        console.log(status);
 
-    // when story is starting to drag
-    task.addEventListener('dragstart', function() {
-        console.log("You are dragging an item");
-        dragTask = task;
-        setTimeout(function () {
+        taskIDs.set(taskName, {id : doc.id});
 
-            task.style.display = 'none';
+        let wholeDiv = document.createElement('div');
+        wholeDiv.draggable = true;
+        wholeDiv.className = "tasks";
 
-        },0);
+        let n = document.createElement('p'); //name comp
+        n.className = "userStoryName";
+        n.innerText= taskName;
+        n.style.marginBottom = "15px";
+
+        let des = document.createElement('p'); //description comp
+        des.className = "acceptance";
+        des.innerText = taskAssigned;
+
+        let btnDeleteUS = document.createElement('button'); // moves to task page
+        btnDeleteUS.className = "userStoryBtn";
+        btnDeleteUS.id = "btnDelete";
+        btnDeleteUS.innerText = "Delete"
+        btnDeleteUS.style.float= "right";
+
+        wholeDiv.appendChild(n);
+        wholeDiv.appendChild(des);
+        wholeDiv.appendChild(btnDeleteUS);
+
+        // attaches user story to the relevant column
+            // 1 : not started
+            // 2 : in progress
+            // 3 : completed
+            if (status === 1) {
+                console.log("in 1");
+
+                NS.appendChild(wholeDiv);
+            }
+
+            else if (status === 2) {
+                console.log("in 2");
+               IP.appendChild(wholeDiv);
+            }
+
+            else if (status === 3) {
+                C.appendChild(wholeDiv);
+            }
+
+            else {
+               NS.appendChild(wholeDiv);
+            }
+
     });
 
-    // when story is stopping drag
-    task.addEventListener('dragend',function () {
-        console.log("You are no longer dragging an item");
-        setTimeout(function () {
+    
+    const tasks = document.querySelectorAll('.tasks');
+    const categories = document.querySelectorAll('.categories');
 
-            task.style.display = 'block';
-            dragTask=null;
+    let dragTask= null;
 
-        },0);
-    });
+    for(let a = 0; a < tasks.length;a++) {
+    const task = tasks[a];
 
-    for (let b = 0; b < categories.length;b++) {
-        const category = categories[b];
+        // when story is starting to drag
+        task.addEventListener('dragstart', function() {
+            console.log("You are dragging an item");
+            dragTask = task;
+            setTimeout(function () {
 
-        category.addEventListener('dragover', function (e) {
+                task.style.display = 'none';
 
-            e.preventDefault();
-
+            },0);
         });
 
-        category.addEventListener('dragenter', function (e) {
-            e.preventDefault();
-            this.style.backgroundColor = '#778899';
+        // when story is stopping drag
+        task.addEventListener('dragend',function () {
+            console.log("You are no longer dragging an item");
+            setTimeout(function () {
 
+                task.style.display = 'block';
+                dragTask=null;
+
+            },0);
         });
 
-        category.addEventListener('dragleave', function () {
-            this.style.backgroundColor = 'whitesmoke';
+        for (let b = 0; b < categories.length;b++) {
+            const category = categories[b];
 
-        })
+            category.addEventListener('dragover', function (e) {
 
-        category.addEventListener('drop',function (ev) {
+                e.preventDefault();
 
-            category.appendChild(dragTask);
-            this.style.backgroundColor = 'whitesmoke';
-        })
+            });
+
+            category.addEventListener('dragenter', function (e) {
+                e.preventDefault();
+                this.style.backgroundColor = '#778899';
+
+            });
+
+            category.addEventListener('dragleave', function () {
+                this.style.backgroundColor = 'whitesmoke';
+
+            })
+
+            category.addEventListener('drop',function (ev) {
+
+                category.appendChild(dragTask);
+                this.style.backgroundColor = 'whitesmoke';
+
+                let parentID = dragTask.parentElement.id;
+
+                let name = dragTask.querySelector('.userStoryName').innerText;
+                let taskID = taskIDs.get(name).id;
+                console.log(taskID);
+
+                if(parentID == "NotStarted"){
+                    console.log(parentID,"1");
+                    db.collection("projects").doc(projectID).collection("sprints").doc(currentSprintID).collection("backlog").doc(userStoryID).collection("tasks").doc(taskID).update({
+                        status: "1"
+                    }).then(function () {
+                        window.location.href = "Task.html";
+                    });
+                }
+                else if(parentID == "In Progress"){
+                    console.log(parentID,"2");
+                    db.collection("projects").doc(projectID).collection("sprints").doc(currentSprintID).collection("backlog").doc(userStoryID).collection("tasks").doc(taskID).update({
+                        status: "2"
+                    }).then(function () {
+                        window.location.href = "Task.html";
+                    });
+
+                }
+                else if(parentID == "Completed"){
+                    console.log(parentID,"3");
+                    db.collection("projects").doc(projectID).collection("sprints").doc(currentSprintID).collection("backlog").doc(userStoryID).collection("tasks").doc(taskID).update({
+                        status: "3"
+                    }).then(function () {
+                        window.location.href = "Task.html";
+                    });
+
+                }
+
+            })
+
+        }
 
     }
-
-}
+});
