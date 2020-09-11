5076c0bdb06cbc44180417d827671cb88fb3ab49
Simphiwe <szulu107@gmail.com>
2020-09-11 14:55:30 +0200

    auth state check added

--- public/public/resources/js/getTasks.js
+++ public/public/resources/js/getTasks.js
@@ -11,34 +11,38 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 // get required values from local storage
-// const auth = firebase.auth();
-// const db = firebase.firestore();
-// const projectID = localStorage.getItem("docID");
-// const ownerID = localStorage.getItem("ownerID");
-// const currentSprintID = localStorage.getItem("currentSprintID");
-// const projectName = localStorage.getItem("docName");
-// const userStoryID = localStorage.getItem("userStoryID");
-// console.log(userStoryID);
+const auth = firebase.auth();
+const db = firebase.firestore();
+const projectID = localStorage.getItem("docID");
+const ownerID = localStorage.getItem("ownerID");
+const currentSprintID = localStorage.getItem("currentSprintID");
+const projectName = localStorage.getItem("docName");
+const userStoryID = localStorage.getItem("userStoryID");
+console.log(userStoryID);
 
-const projectTitle = document.querySelector("#PageHeading");
-let heading = `<p>Project: ${projectName}</p>`;
-projectTitle.innerHTML = heading;
 
-const sprintHeading = document.querySelector('#subPageHeading');
-db.collection("projects").doc(projectID).collection("sprints").doc(currentSprintID).get().then((doc) => {
-    let html = `<p>Sprint: ${doc.data().name}</p>`;
-    sprintHeading.innerHTML = html;
-});
-
-const gitLink = document.querySelector('#gitLink');
-db.collection("projects").doc(projectID)
-    .onSnapshot(function (doc) {
-        let git = `<a href="${doc.data().repository}">Git</a>`;
-        gitLink.innerHTML = git;
-    });
 
 // display pending invites
 auth.onAuthStateChanged(user => {
+
+    if (user) {
+        const projectTitle = document.querySelector("#PageHeading");
+    let heading = `<p>Project: ${projectName}</p>`;
+    projectTitle.innerHTML = heading;
+
+    const sprintHeading = document.querySelector('#subPageHeading');
+    db.collection("projects").doc(projectID).collection("sprints").doc(currentSprintID).get().then((doc) => {
+        let html = `<p>Sprint: ${doc.data().name}</p>`;
+        sprintHeading.innerHTML = html;
+    });
+
+    const gitLink = document.querySelector('#gitLink');
+    db.collection("projects").doc(projectID)
+        .onSnapshot(function (doc) {
+            let git = `<a href="${doc.data().repository}">Git</a>`;
+            gitLink.innerHTML = git;
+        });
+
     const inviteBadge = document.querySelector("#inviteBadge");
     db.collection("Invites").where("inviteToID", "==", user.uid)
         .onSnapshot(function (snapshot) {
@@ -51,6 +55,184 @@ auth.onAuthStateChanged(user => {
                 inviteBadge.innerHTML = html2;
             }
         });
+    //Drag & Drop Code
+
+    const NS = document.getElementById("ns-tasks");
+    const IP = document.getElementById("ip-tasks");
+    const C = document.getElementById("c-tasks");
+
+    let wholeDiv;
+    let taskIDs = new Map();
+
+    //get tasks from db
+
+    db.collection("projects").doc(projectID).collection("sprints").doc(currentSprintID).collection("backlog").doc(userStoryID).collection("tasks").get().then(function (querySnapshot) {
+        querySnapshot.forEach(function (doc) {
+
+            let taskName = doc.data().name;
+            let taskAssigned = doc.data().assigned_to;
+            let status = parseInt(doc.data().status);
+            console.log(status);
+
+            taskIDs.set(taskName, {
+                id: doc.id
+            });
+
+            let wholeDiv = document.createElement('div');
+            wholeDiv.draggable = true;
+            wholeDiv.className = "tasks";
+
+            let n = document.createElement('p'); //name comp
+            n.className = "userStoryName";
+            n.innerText = taskName;
+            n.style.marginBottom = "15px";
+
+            let des = document.createElement('p'); //description comp
+            des.className = "acceptance";
+            des.innerText = taskAssigned;
+
+            let btnDeleteUS = document.createElement('button'); // moves to task page
+            btnDeleteUS.className = "userStoryBtn";
+            btnDeleteUS.id = "btnDelete";
+            btnDeleteUS.innerText = "Delete"
+            btnDeleteUS.style.float = "right";
+
+
+            wholeDiv.appendChild(n);
+            wholeDiv.appendChild(des);
+            wholeDiv.appendChild(btnDeleteUS);
+
+            // attaches user story to the relevant column
+            // 1 : not started
+            // 2 : in progress
+            // 3 : completed
+            if (status === 1) {
+                console.log("in 1");
+
+                NS.appendChild(wholeDiv);
+            } else if (status === 2) {
+                console.log("in 2");
+                IP.appendChild(wholeDiv);
+            } else if (status === 3) {
+                C.appendChild(wholeDiv);
+            } else {
+                NS.appendChild(wholeDiv);
+            }
+
+        });
+
+
+        const tasks = document.querySelectorAll('.tasks');
+        const categories = document.querySelectorAll('.categories');
+
+        let dragTask = null;
+
+        for (let a = 0; a < tasks.length; a++) {
+            const task = tasks[a];
+
+            let deleteT = task.querySelector("#btnDelete");
+            deleteT.addEventListener('click', function () {
+                let name = task.querySelector(".userStoryName").innerText;
+                let deleteID = taskIDs.get(name).id;
+                db.collection("projects").doc(projectID).collection("sprints").doc(currentSprintID).collection("backlog").doc(userStoryID).collection("tasks").doc(deleteID).delete().then(function () {
+                    console.log("Document successfully deleted!");
+                    window.location.href = "Task.html";
+                }).catch(function (error) {
+                    console.error("Error removing document: ", error);
+                });
+                console.log(deleteID);
+            });
+
+
+            // when story is starting to drag
+            task.addEventListener('dragstart', function () {
+                console.log("You are dragging an item");
+                dragTask = task;
+                setTimeout(function () {
+
+                    task.style.display = 'none';
+
+                }, 0);
+            });
+
+            // when story is stopping drag
+            task.addEventListener('dragend', function () {
+                console.log("You are no longer dragging an item");
+                setTimeout(function () {
+
+                    task.style.display = 'block';
+                    dragTask = null;
+
+                }, 0);
+            });
+
+            for (let b = 0; b < categories.length; b++) {
+                const category = categories[b];
+
+                category.addEventListener('dragover', function (e) {
+
+                    e.preventDefault();
+
+                });
+
+                category.addEventListener('dragenter', function (e) {
+                    e.preventDefault();
+                    this.style.backgroundColor = '#778899';
+
+                });
+
+                category.addEventListener('dragleave', function () {
+                    this.style.backgroundColor = 'whitesmoke';
+
+                })
+
+                category.addEventListener('drop', function (ev) {
+
+                    category.appendChild(dragTask);
+                    this.style.backgroundColor = 'whitesmoke';
+
+                    let parentID = dragTask.parentElement.id;
+
+                    let name = dragTask.querySelector('.userStoryName').innerText;
+                    let taskID = taskIDs.get(name).id;
+                    console.log(taskID);
+
+                    if (parentID == "NotStarted") {
+                        console.log(parentID, "1");
+                        db.collection("projects").doc(projectID).collection("sprints").doc(currentSprintID).collection("backlog").doc(userStoryID).collection("tasks").doc(taskID).update({
+                            status: "1"
+                        }).then(function () {
+                            window.location.href = "Task.html";
+                        });
+                    } else if (parentID == "In Progress") {
+                        console.log(parentID, "2");
+                        db.collection("projects").doc(projectID).collection("sprints").doc(currentSprintID).collection("backlog").doc(userStoryID).collection("tasks").doc(taskID).update({
+                            status: "2"
+                        }).then(function () {
+                            window.location.href = "Task.html";
+                        });
+
+                    } else if (parentID == "Completed") {
+                        console.log(parentID, "3");
+                        db.collection("projects").doc(projectID).collection("sprints").doc(currentSprintID).collection("backlog").doc(userStoryID).collection("tasks").doc(taskID).update({
+                            status: "3"
+                        }).then(function () {
+                            window.location.href = "Task.html";
+                        });
+
+                    }
+
+                })
+
+            }
+
+        }
+    });
+    } else {
+        console.log("user logged out");
+        window.location.href = "index.html";
+    }
+    
 });
 
 
@@ -61,190 +243,10 @@ logout.addEventListener("click", (e) => {
 
     auth.signOut().then(() => {
 
-        window.location.href = "index.html";
-    })
+            window.location.href = "index.html";
+        })
         .catch(function (error) {
             console.log("user failed to sign out because of error: ", error);
             alert(error.message);
         });
-});
-
-//Drag & Drop Code
-
-const NS = document.getElementById("NotStarted");
-const IP= document.getElementById("In Progress");
-const C = document.getElementById("Completed");
-
-let wholeDiv;
-let taskIDs = new Map();
-
-//get tasks from db
-
-db.collection("projects").doc(projectID).collection("sprints").doc(currentSprintID).collection("backlog").doc(userStoryID).collection("tasks").get().then(function(querySnapshot) {
-    querySnapshot.forEach(function(doc) {
-
-        let taskName = doc.data().name;
-        let taskAssigned = doc.data().assigned_to;
-        let status = parseInt(doc.data().status);
-        console.log(status);
-
-        taskIDs.set(taskName, {id : doc.id});
-
-        let wholeDiv = document.createElement('div');
-        wholeDiv.draggable = true;
-        wholeDiv.className = "tasks";
-
-        let n = document.createElement('p'); //name comp
-        n.className = "userStoryName";
-        n.innerText= taskName;
-        n.style.marginBottom = "15px";
-
-        let des = document.createElement('p'); //description comp
-        des.className = "acceptance";
-        des.innerText = taskAssigned;
-
-        let btnDeleteUS = document.createElement('button'); // moves to task page
-        btnDeleteUS.className = "userStoryBtn";
-        btnDeleteUS.id = "btnDelete";
-        btnDeleteUS.innerText = "Delete"
-        btnDeleteUS.style.float= "right";
-        
-
-        wholeDiv.appendChild(n);
-        wholeDiv.appendChild(des);
-        wholeDiv.appendChild(btnDeleteUS);
-
-        // attaches user story to the relevant column
-            // 1 : not started
-            // 2 : in progress
-            // 3 : completed
-            if (status === 1) {
-                console.log("in 1");
-
-                NS.appendChild(wholeDiv);
-            }
-
-            else if (status === 2) {
-                console.log("in 2");
-               IP.appendChild(wholeDiv);
-            }
-
-            else if (status === 3) {
-                C.appendChild(wholeDiv);
-            }
-
-            else {
-               NS.appendChild(wholeDiv);
-            }
-
-    });
-
-    
-    const tasks = document.querySelectorAll('.tasks');
-    const categories = document.querySelectorAll('.categories');
-
-    let dragTask= null;
-
-    for(let a = 0; a < tasks.length;a++) {
-    const task = tasks[a];
-
-        let deleteT = task.querySelector("#btnDelete");
-        deleteT.addEventListener('click', function () {
-            let name = task.querySelector(".userStoryName").innerText;
-            let deleteID = taskIDs.get(name).id;
-            db.collection("projects").doc(projectID).collection("sprints").doc(currentSprintID).collection("backlog").doc(userStoryID).collection("tasks").doc(deleteID).delete().then(function () {
-                console.log("Document successfully deleted!");
-                window.location.href = "Task.html";
-            }).catch(function (error) {
-                console.error("Error removing document: ", error);
-            });
-            console.log(deleteID);
-        });
-
-
-        // when story is starting to drag
-        task.addEventListener('dragstart', function() {
-            console.log("You are dragging an item");
-            dragTask = task;
-            setTimeout(function () {
-
-                task.style.display = 'none';
-
-            },0);
-        });
-
-        // when story is stopping drag
-        task.addEventListener('dragend',function () {
-            console.log("You are no longer dragging an item");
-            setTimeout(function () {
-
-                task.style.display = 'block';
-                dragTask=null;
-
-            },0);
-        });
-
-        for (let b = 0; b < categories.length;b++) {
-            const category = categories[b];
-
-            category.addEventListener('dragover', function (e) {
-
-                e.preventDefault();
-
-            });
-
-            category.addEventListener('dragenter', function (e) {
-                e.preventDefault();
-                this.style.backgroundColor = '#778899';
-
-            });
-
-            category.addEventListener('dragleave', function () {
-                this.style.backgroundColor = 'whitesmoke';
-
-            })
-
-            category.addEventListener('drop',function (ev) {
-
-                category.appendChild(dragTask);
-                this.style.backgroundColor = 'whitesmoke';
-
-                let parentID = dragTask.parentElement.id;
-
-                let name = dragTask.querySelector('.userStoryName').innerText;
-                let taskID = taskIDs.get(name).id;
-                console.log(taskID);
-
-                if(parentID == "NotStarted"){
-                    console.log(parentID,"1");
-                    db.collection("projects").doc(projectID).collection("sprints").doc(currentSprintID).collection("backlog").doc(userStoryID).collection("tasks").doc(taskID).update({
-                        status: "1"
-                    }).then(function () {
-                        window.location.href = "Task.html";
-                    });
-                }
-                else if(parentID == "In Progress"){
-                    console.log(parentID,"2");
-                    db.collection("projects").doc(projectID).collection("sprints").doc(currentSprintID).collection("backlog").doc(userStoryID).collection("tasks").doc(taskID).update({
-                        status: "2"
-                    }).then(function () {
-                        window.location.href = "Task.html";
-                    });
-
-                }
-                else if(parentID == "Completed"){
-                    console.log(parentID,"3");
-                    db.collection("projects").doc(projectID).collection("sprints").doc(currentSprintID).collection("backlog").doc(userStoryID).collection("tasks").doc(taskID).update({
-                        status: "3"
-                    }).then(function () {
-                        window.location.href = "Task.html";
-                    });
-
-                }
-
-            })
-
-        }
-
-    }
 });
