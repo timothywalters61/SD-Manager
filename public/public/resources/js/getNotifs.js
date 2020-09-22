f0c23256f8e3626799a17bcbf44e9b53758f5aff
phediso99 <61417155+phediso99@users.noreply.github.com>
2020-09-22 20:01:13 +0200

    Update getNotifs.js

--- public/public/resources/js/getNotifs.js
+++ public/public/resources/js/getNotifs.js
@@ -16,41 +16,57 @@ const db = firebase.firestore();
 auth.onAuthStateChanged(user => {
     const inviteBadge = document.querySelector("#inviteBadge");
     db.collection("Invites").where("inviteToID", "==", user.uid)
-        .onSnapshot(function (snapshot) {
+        .onSnapshot(function(snapshot) {
             if (snapshot.docs != 0) {
-            let html = `<span class="badge">${snapshot.docs.length}</span> Invites`;
-            inviteBadge.innerHTML = html;
-        } else {
-            console.log("invites do not exist");
-            let html2 = '<span class="badge">0</span> Invites';
-            inviteBadge.innerHTML = html2;
-        }
-    });
+                let html = `<span class="badge">${snapshot.docs.length}</span> Invites`;
+                inviteBadge.innerHTML = html;
+            } else {
+                console.log("invites do not exist");
+                let html2 = '<span class="badge">0</span> Invites';
+                inviteBadge.innerHTML = html2;
+            }
+        });
 });
 
 auth.onAuthStateChanged(user => {
     if (user) {
         console.log("user logged in: ", user);
-        
-        db.collection("users").doc(user.uid).get().then(function(doc){
+
+        db.collection("users").doc(user.uid).get().then(function(doc) {
 
             let notifs = doc.data().notifications;
             console.log(notifs);
-            localStorage.setItem("notifications" , notifs);
+            localStorage.setItem("notifications", notifs);
 
-         const notifDiv = document.getElementById("Notifications");
+            const notifDiv = document.getElementById("Notifications");
 
-            
-            for(let i = 0; i < notifs.length; i++){
+
+            for (let i = 0; i < notifs.length; i++) {
                 console.log(notifs[i]);
 
                 let nDiv = document.createElement("div");
+
                 nDiv.innerText = notifs[i];
                 notifDiv.appendChild(nDiv);
                 //nDiv.style.padding = "5px";
                 nDiv.style.margin = "10px";
                 nDiv.style.backgroundColor = "white";
                 nDiv.style.borderRadius = "10px";
+
+                let btnDelete = document.createElement("button");
+                nDiv.appendChild(btnDelete)
+                btnDelete.className = "deleteBtn";
+                btnDelete.id = "btnDeleteDiv";
+                btnDelete.innerText = "delete";
+                btnDelete.style.float = "right";
+
+                /*btnDelete.style.margin = "0px";
+                btnDelete.style.backgroundColor = "blue";
+                btnDelete.style.color = "white";
+                btnDelete.style.blockSize = "20px";
+                btnDelete.style.*/
+
+
 
             }
 
@@ -72,9 +88,9 @@ logout.addEventListener("click", (e) => {
     e.preventDefault();
 
     auth.signOut().then(() => {
-        //console.log("user has signed out");
-    })
-        .catch(function (error) {
+            //console.log("user has signed out");
+        })
+        .catch(function(error) {
             console.log("user failed to sign out because of error: ", error);
             alert(error.message);
         });
