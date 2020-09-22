bdd61f1907df0c5098a02836f431039a60325fd9
Simphiwe <szulu107@gmail.com>
2020-09-22 14:22:04 +0200

    Changes thus far

--- public/public/resources/js/loggedInUserHome.js
+++ public/public/resources/js/loggedInUserHome.js
@@ -1,11 +1,11 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyB1akwPd-xOMCgU9_Bc6OqdTObTp10Sb5k",
-    authDomain: "scrum-manager-91e13.firebaseapp.com",
-    databaseURL: "https://scrum-manager-91e13.firebaseio.com",
-    projectId: "scrum-manager-91e13",
-    storageBucket: "scrum-manager-91e13.appspot.com",
-    messagingSenderId: "332929508306",
-    appId: "1:332929508306:web:5f1773dc956813db641e0b"
+    apiKey: 'AIzaSyB1akwPd-xOMCgU9_Bc6OqdTObTp10Sb5k',
+    authDomain: 'scrum-manager-91e13.firebaseapp.com',
+    databaseURL: 'https://scrum-manager-91e13.firebaseio.com',
+    projectId: 'scrum-manager-91e13',
+    storageBucket: 'scrum-manager-91e13.appspot.com',
+    messagingSenderId: '332929508306',
+    appId: '1:332929508306:web:5f1773dc956813db641e0b',
 };
 
 firebase.initializeApp(firebaseConfig);
@@ -13,75 +13,87 @@ firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.firestore();
 
-auth.onAuthStateChanged(user => {
+auth.onAuthStateChanged((user) => {
     if (user) {
-        console.log("user logged in: ", user);
+        console.log('user logged in: ', user);
         //create user doc in database
 
-        db.collection("users").doc(user.uid)
+        db.collection('users')
+            .doc(user.uid)
             .get()
             .then((doc) => {
                 if (doc.exists) {
-                    console.log("user document exists");
+                    console.log('user document exists');
                 } else {
-                    db.collection("users").doc(user.uid).set({
-                        userEmail: user.email,
-                        userDisplayName: user.displayName
-                    }).then(() => {
-
-                    }).catch(function (error) {
-                        alert("user document failed to create because of error: ", error);
-                    });
+                    db.collection('users')
+                        .doc(user.uid)
+                        .set({
+                            userEmail: user.email,
+                            userDisplayName: user.displayName,
+                        })
+                        .then(() => {})
+                        .catch(function (error) {
+                            alert(
+                                'user document failed to create because of error: ',
+                                error
+                            );
+                        });
                 }
             });
 
         //set up projects
 
-        db.collection("projects").where("Team", "array-contains", user.email)
+        db.collection('projects')
+            .where('Team', 'array-contains', user.email)
             .onSnapshot(function (snapshot) {
                 if (snapshot.docs != 0) {
                     setUpProjects(snapshot.docs);
                 } else {
-                    console.log("projects do not exist");
-                    const projectBox = document.querySelector("#projectContainer");
-                    let html = '<div class="project-box"><a href="#"></a><span>No Projects Yet!!!</span></div>';
+                    console.log('projects do not exist');
+                    const projectBox = document.querySelector(
+                        '#projectContainer'
+                    );
+                    let html =
+                        '<div class="project-box"><a href="#"></a><span>No Projects Yet!!!</span></div>';
                     projectBox.innerHTML = html;
                 }
             });
 
         //set up invite badge
 
-        const inviteBadge = document.querySelector("#inviteBadge");
+        const inviteBadge = document.querySelector('#inviteBadge');
 
-        db.collection("Invites").where("inviteToID", "==", user.uid)
+        db.collection('Invites')
+            .where('inviteToID', '==', user.uid)
             .onSnapshot(function (snapshot) {
                 if (snapshot.docs != 0) {
                     let html = `<span class="badge">${snapshot.docs.length}</span> Invites`;
                     inviteBadge.innerHTML = html;
+                    localStorage.setItem('invites', snapshot.docs.length);
                 } else {
-                    console.log("invites do not exist");
+                    console.log('invites do not exist');
                     let html2 = '<span class="badge">0</span> Invites';
                     inviteBadge.innerHTML = html2;
                 }
             });
-
     } else {
-        console.log("user logged out");
-        window.location.href = "index.html";
+        console.log('user logged out');
+        window.location.href = 'index.html';
     }
 });
 
 //logout
 
-const logout = document.querySelector("#logout");
-logout.addEventListener("click", (e) => {
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) => {
     e.preventDefault();
 
-    auth.signOut().then(() => {
-        //console.log("user has signed out");
-    })
+    auth.signOut()
+        .then(() => {
+            //console.log("user has signed out");
+        })
         .catch(function (error) {
-            console.log("user failed to sign out because of error: ", error);
+            console.log('user failed to sign out because of error: ', error);
             alert(error.message);
         });
 });
--- public/public/resources/js/messages.js
+++ public/public/resources/js/messages.js
@@ -1,3 +1,146 @@
 /**
  * @description This file contains all messaging functionality
  */
+
+const auth = firebase.auth();
+const db = firebase.firestore();
+const invites = localStorage.getItem('invites');
+const projectID = localStorage.getItem('docID');
+const projectName = localStorage.getItem('docName');
+const ref = db.collection('projects').doc(projectID).collection('messages');
+var username = '';
+
+auth.onAuthStateChanged((user) => {
+    if (user) {
+        // User is logged in
+
+        // Project title setup
+        const projectTitle = document.querySelector('#PageHeading');
+        let heading = `<p>Project: ${projectName}</p>`;
+        projectTitle.innerHTML = heading;
+
+        // Invite badge setup
+        const inviteBadge = document.querySelector('#inviteBadge');
+        let html = `<span class="badge">${invites}</span> Invites`;
+        inviteBadge.innerHTML = html;
+
+        db.collection('projects')
+            .doc(projectID)
+            .onSnapshot((snapshot) => {
+                if (snapshot.exists) {
+                    var data = snapshot.data();
+
+                    if (!data.Team.includes(user.email)) {
+                        console.log('User is not a member.');
+                        window.location.href = 'userHome.html';
+                    }
+
+                    //git and links
+
+                    const gitLink = document.querySelector('#gitLink');
+                    let link = `${data.repository}`;
+                    console.log(link);
+                    let git = `<a href="${link}">Git</a>`;
+
+                    gitLink.innerHTML = git;
+                    console.log(gitLink.innerHTML);
+                } else {
+                    console.log('Project does not exist');
+                    window.location.href = 'userHome.html';
+                }
+            });
+        db.collection('users')
+            .doc(user.uid)
+            .get()
+            .then((snapshot) => {
+                if (snapshot.exists) {
+                    var data = snapshot.data();
+                    username = data.displayName;
+                    loadMessages(data.displayName);
+                }
+            });
+    } else {
+        // User is not logged in
+        console.log('user logged out');
+        window.location.href = 'index.html';
+    }
+});
+
+/**
+ * Returns text that will be used as the innerHTML of a message div
+ * @param {String} text
+ */
+const createMessageInnerHtml = (sender, text) => {
+    var div = `<div class="sender">${sender}</div><div class="message-text">${text}</div>`;
+    return div;
+};
+
+/**
+ * Returns a div that will be used to display a message
+ * @param {String} id
+ * @param {String} sender
+ * @param {String} sent
+ * @param {String} text
+ */
+const createMessageDiv = (id, sender, sent, text) => {
+    var div = document.createElement('div');
+    div.setAttribute('id', id);
+    if (sent) {
+        div.classList.add('message-sent');
+    } else {
+        div.classList.add('message-received');
+    }
+    div.innerHTML = createMessageInnerHtml(sender, text);
+    return div;
+};
+
+/**
+ *
+ * @param {String} id
+ * @param {String} sender
+ * @param {String} sent
+ * @param {String} text
+ */
+const displayMessage = (id, sender, sent, text) => {
+    if (id != 'placeholder') {
+        document.querySelector('#placeholder').classList.remove('visible');
+    }
+    var div = document.querySelector('#' + id);
+    let chat = document.querySelector('#message-container');
+    if (!div) {
+        div = createMessageDiv(id, sender, sent, text);
+    }
+
+    setTimeout(() => {
+        div.classList.add('visible');
+    }, 1);
+    chat.scrollTop = chat.scrollHeight;
+};
+
+/**
+ * Loads messages from the group chat
+ * @param {String} username
+ */
+const loadMessages = (username) => {
+    // Remove all previous event listeners
+    // ref.off();
+
+    // Load the last 20 messages from the chat
+    const setMessage = (query) => {
+        if (!query.empty) {
+            query.forEach((doc) => {
+                var sent = false;
+                if (data.sender == username) {
+                    sent = true;
+                }
+                displayMessage(id, data.sender, sent, data.message);
+            });
+        } else {
+            document.querySelector('#placeholder').classList.add('visible');
+        }
+    };
+
+    ref.orderBy('time', 'asc').limitToLast(20).onSnapshot(setMessage);
+};
+
+const saveMessage = () => {};
